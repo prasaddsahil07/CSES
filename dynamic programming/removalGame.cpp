454bf64d440d2fda46b2d0fef27aafe450752cf1
Sahil Prasad <sahilprasad41969@gmailcom>
2025-08-08 11:37:47 +0530

    added one problem

--- dynamic programming/removalGame.cpp
+++ dynamic programming/removalGame.cpp
@@ -1,45 +1,50 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
+// typedef long long ll;
 
-typedef long long ll;
+// ll solve(vector<ll>& nums, int i, int j, int player, vector<vector<vector<ll>>>& dp){
+//     if(i > j){
+//         return 0;
+//     }
+//     if(dp[i][j][player] != -1){
+//         return dp[i][j][player];
+//     }
 
-ll solve(vector<ll>& nums, int i, int j, int player, vector<vector<ll>>& dp){
-    if(i > j){
-        return 0;
-    }
-    if(dp[i][j] != -1){
-        return dp[i][j];
-    }
-    ll score = (player == 0)? LLONG_MIN: LLONG_MAX;
-    if(player == 0){
-        ll firstPick = nums[i] + solve(nums, i+1, j, 1, dp);
-        ll lastPick = nums[j] + solve(nums, i, j-1, 1, dp);
-        score = max({score, firstPick, lastPick});
-    }
-    else{
-        ll firstPick = solve(nums, i+1, j, 0, dp);
-        ll lastPick = solve(nums, i, j-1, 0, dp);
-        score = min({score, firstPick, lastPick});
-    }
-    return dp[i][j] = score;
-}
+//     // for player 0, we want to maximize the score
+//     // for player 1, we want to minimize the score
+//     ll score = (player == 0)? -10e17: 10e17;
 
-int main(){
-    int n;
-    cin>>n;
-    vector<ll> vec(n);
-    for(auto& v: vec){
-        cin>>v;
-    } 
-    vector<vector<ll>> dp(n, vector<ll>(n, -1));
-    cout << solve(vec, 0, n-1, 0, dp) << endl;
-}
+//     if(player == 0){
+//         ll firstPick = nums[i] + solve(nums, i+1, j, 1, dp);
+//         ll lastPick = nums[j] + solve(nums, i, j-1, 1, dp);
+//         score = max({score, firstPick, lastPick});
+//     }
+//     else{
+//         ll firstPick = solve(nums, i+1, j, 0, dp);
+//         ll lastPick = solve(nums, i, j-1, 0, dp);
+//         score = min({score, firstPick, lastPick});
+//     }
+//     return dp[i][j][player] = score;
+// }
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     vector<ll> vec(n);
+//     for(auto& v: vec){
+//         cin>>v;
+//     } 
+
+//     // dp[i][j][player] will store the maximum score for the player starting at index i to j
+//     vector<vector<vector<ll>>> dp(n, vector<vector<ll>>(n, vector<ll>(2, -1)));
+//     cout<<solve(vec, 0, n-1, 0, dp)<<endl;
+// }
 
 
 
 
 
-/*
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -59,26 +64,28 @@ ll solve(vector<ll>& nums) {
     for (int length = 2; length <= n; ++length) {
         for (int i = 0; i <= n - length; ++i) {
             int j = i + length - 1; // End of the subarray
+
             // If player 1 picks nums[i], the remaining range is nums[i+1..j], and player 2 plays optimally
-            ll pickFirst = nums[i] + min((i + 2 <= j) ? dp[i + 2][j] : 0, (i + 1 <= j - 1) ? dp[i + 1][j - 1] : 0);
+            ll pickFirst = nums[i] + min((i+2 <= j)? dp[i+2][j]: 0, (i+1 <= j-1)? dp[i+1][j-1]: 0);
+
             // If player 1 picks nums[j], the remaining range is nums[i..j-1], and player 2 plays optimally
-            ll pickLast = nums[j] + min((i <= j - 2) ? dp[i][j - 2] : 0, (i + 1 <= j - 1) ? dp[i + 1][j - 1] : 0);
+            ll pickLast = nums[j] + min((i <= j-2) ? dp[i][j-2]: 0, (i+1 <= j-1)? dp[i+1][j-1]: 0);
+
             // The first player will choose the option that maximizes their score
             dp[i][j] = max(pickFirst, pickLast);
         }
     }
 
     // The result for the entire array is stored in dp[0][n-1]
-    return dp[0][n - 1];
+    return dp[0][n-1];
 }
 
 int main() {
     int n;
     cin >> n;
     vector<ll> vec(n);
-    for (auto& v : vec) {
-        cin >> v;
+    for(auto& v: vec){
+        cin>>v;
     } 
-    cout << solve(vec) << endl;
+    cout<<solve(vec)<<endl;
 }
-*/
